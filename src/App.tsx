e7584abc196cfeec77724881bbdd7f00f345051d
Nuntavat <nuntavat_p@hotmail.com>
2025-05-21 14:14:55 +0700

    update button

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
 
         </a>
       </div>
-      <h1>E-Com System 1.0.0 (add login page)</h1>
+      <h1>E-Com System 1.0.0 (add login page) (update label cart button)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

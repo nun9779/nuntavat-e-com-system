98e9cf15cce2acdb06702022ab3ee43c3ae844e4
Nuntavat <nuntavat_p@hotmail.com>
2025-05-21 15:14:01 +0700

    NUN-1 add grid ui

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
 
         </a>
       </div>
-      <h1>E-Com System 1.0.0 (add login page) (update label cart button)</h1>
+      <h1>E-Com System 1.0.0 (add login page) (update label cart button) (add grid ui)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
